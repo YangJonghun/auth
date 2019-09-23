c59b7e5793e8b52f6d64743b67fc5f259488001e
Joonyoung Shin <joonyoungshin@Joonyoungui-MacBookPro.local>
2019-09-23 17:10:32 +0900

    server init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,52 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import Koa from "koa";
+import Router from '@koa/router';
+import { ApolloServer } from "apollo-server-koa";
+// import {createConnection} from "typeorm";
+// import {User} from "./entity/User";
 
-createConnection().then(async connection => {
+(async () => {
+    const app = new Koa();
+    const router = new Router();
+    
+    const apolloServer = new ApolloServer({
+        typeDefs: `
+        type Query {
+            hello: String!
+        }
+        `,
+        resolvers: {
+            Query: {
+                hello: () => 'hello world'
+            }
+        }
+    })
+    
+    apolloServer.applyMiddleware({ app });
+    
+    router.get('/', (ctx, _next) => ctx.body = 'hello');
+    
+    app.use(router.routes()).use(router.allowedMethods());
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+    app.listen(4000, () => {
+        console.log('express server started')
+    });
+})()
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+// createConnection().then(async connection => {
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
 
-}).catch(error => console.log(error));
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
